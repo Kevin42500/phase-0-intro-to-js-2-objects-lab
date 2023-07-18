6a5fc728f8c79a3f1b7869427a6a5b35b0395813
scobett <kevin@Scobett>
2023-07-18 18:30:03 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+const employee = {
+    name: "John",
+    age: 30,
+    role: "Engineer",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee, [key]: value };
+    return updatedEmployee;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  console.log(updateEmployeeWithKeyAndValue(employee, "age", 32));
+  console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "role", "Manager"));
+  console.log(deleteFromEmployeeByKey(employee, "name"));
+  console.log(destructivelyDeleteFromEmployeeByKey(employee, "age"));
+  
+  module.exports = {
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey,
+  };
+  
